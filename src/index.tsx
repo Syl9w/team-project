35039bde1fae21b3aec1d30035e0f66efd6bd538
leanderfitze <leanderfitze@gmail.com>
2023-06-04 23:29:25 +0200

    add rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/index.css'
 import reportWebVitals from './reportWebVitals'
 import { StoreContext, store } from './app/stores/store'
+import App from './app/layout/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
